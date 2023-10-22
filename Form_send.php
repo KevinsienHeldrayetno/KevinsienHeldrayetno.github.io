e82f88c6e9a070243bc845bad48629f4529036fa
KevinsienHeldrayetno <kevinsien.120140174@student.itera.ac.id>
2023-10-23 01:26:53 +0700

    initial commit

---
+++ Form_send.php
@@ -0,0 +1,10 @@
+<?php
+    echo $_GET['name'];
+    echo $_GET['nim'];
+    echo $_GET['prodi'];
+    echo $_GET['email'];
+    echo $_GET['pass'];
+    echo $_GET['gender'];
+    echo $_GET['alamat'];
+    echo $_GET['bday'];
+?>
